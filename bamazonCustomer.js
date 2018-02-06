d6960c278a0f33e4823450ff1e54a36216331217
El Pirata Mike <mikefields81@gmail.com>
2018-02-06 12:42:57 -0600

    products table updated, file saved and opening inventory console logged

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,4 +16,19 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  console.log("These are the items currently for sale...")
+
+	var query = "SELECT * FROM products";
+  	connection.query(query, function(err, res) {
+  		// console.log(res);
+  		// var r = res[i];
+  		console.log("Item ID | Product Name | Product Price\n-------------------------------------");
+
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + " | ");
+    };
+
+  });
+
 });
+	
